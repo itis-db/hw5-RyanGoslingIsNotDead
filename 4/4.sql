0fa3d748cb169cffcf20221ee302de728fbb6fb9
Kadim Arslanov <144515768+RyanGoslingIsNotDead@users.noreply.github.com>
2025-06-20 11:04:04 +0300

    Delete 4 directory

--- 4/4.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  task_id,
-  project_id,
-  start_date,
-  MIN(start_date) OVER (PARTITION BY project_id) AS first_task_start,
-  MAX(start_date) OVER (PARTITION BY project_id) AS last_task_start
-FROM project_tasks;
